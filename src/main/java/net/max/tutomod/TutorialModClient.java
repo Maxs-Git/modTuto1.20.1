79fa6f39fbbac63dc8e1255c0e2e830c50096542
max <maxetness@gmail.com>
2023-11-16 11:50:56 +0100

    Initial commit

---
+++ src/main/java/net/max/tutomod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.max.tutomod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
